fd20f7b2530ecb41b1a015b102cdd1af8c96f5b8
Sheyilaaw <Sheyilaaw98@gmail.com>
2018-03-13 21:17:11 +0100

    Merge and implement saving of plant to database

---
+++ ajax/saveInventory.php
@@ -0,0 +1,13 @@
+<?php
+if(sizeof($_POST)>0){
+    $type = (int) $_POST['type'];
+    if($type==0){
+        $plantName  =   $_POST['plantName'];
+        $quantity   =   $_POST['quantity'];
+        //Save to Db;
+    }elseif($type==1){
+        //Livestock
+        $animalName =   $_POST['animalName'];
+        $quantity   =   $_POST['animalQty'];
+    }
+}

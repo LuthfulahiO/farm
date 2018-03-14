d2f1a01465d5c6c998f0856dc4186b787f6ba337
Sheyilaaw <Sheyilaaw98@gmail.com>
2018-03-14 01:16:03 +0100

    added extra files to load

--- includes/initialize.php
+++ includes/initialize.php
@@ -1,6 +1,6 @@
 <?php
 // load config file first
 require_once('config.php');
-
+require_once('functions.php');
 // load api functions next so that everything after can use them
 require_once('ClearApi.php');
